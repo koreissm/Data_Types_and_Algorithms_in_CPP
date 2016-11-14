da1016b65b22995a7d7e55302d1cdc8060394028
Mouhamad <koreissimouhamad@outlook.fr>
2016-11-14 21:42:55 +0100

    Implemented selection sorting

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Data_Types_in_C
-Implementation of most known Data types in C
+# Data Types and Algorithms in C++
+
+Why ?
+
+The goal is to implement the most known data types and algorithms in C++ so to master them better. Any suggestion of improvement is welcome.
+
+Here are the fields that will be covered :
+    - Linked-Lists, Queues & Stacks
+    - Trees & Heaps
+    - Searching (linear, binary)
+    - Sorting (selection, inertion, bubble, quick, merge)
+    - Strings' Manipulation
--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-int nbElements = -1; 
-
 template <typename T>
 void printArray(const T *array, const int &size) {
     int i;
@@ -15,46 +13,42 @@ void printArray(const T *array, const int &size) {
     cout << "\n";
 }
 
-template <typename T>
-T select(T *array, int begin, int end) {
-    int selected = MIN;
-    int i;
-
-    for (i = begin; i < end; i++) {
-        if (array[i] < selected)
-            selected = array[i];
-    }
-
-    return selected;
+//Swapping two values
+void swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
 }
 
 template <typename T>
-void insert(T *array, int element) {
-    if (nbElements >= SIZE) return; //Return if overflowed
-
-    nbElements++;
-    int i;
-
-    //Shifting all elements to right until element found
-    for (i = nbElements; i >= 0; i--) {
-        if (array[i - 1] > element)
-            array[i] = array[i - 1];
-        else {
-            array[i] = element;
-            break;
+void selectionSort(T *array, int size) {
+    int i, j;
+    for (i = 0; i < size; i++) {
+        //Searching for the min in the right of the array
+        int min = MIN;
+        int index = -1;
+        for (j = i; j < size; j++) {
+            if (array[j] < min) {
+                min = array[j];
+                index = j;
+            }
         }
+
+        //Swapping
+        swap(array[i], array[index]);
     }
 }
 
 int main() {
+    int array[8] = {6, 3, 9, 8, 1, 5, 4, 2};
+    printf("Array before sorting : ");
+    printArray(array, 8);
 
-    //int array[10] = {6, 3, 9, 8, 1, 5, 4, 2, 0, 0};
-    int array[10] = {3, 5, 8, 9, 0, 0};
-//    printf("begin : %d, end : %d, selected : %d\n", 5, 8, select(array, 5, 8));
-    nbElements = 3;
-    insert(array, 7);
-    insert(array, 1);
-    insert(array, 1);
-    printArray(array, 10);
+    //Sorting the array of size 8
+    selectionSort(array, 8);
+
+    printf("Array after being sorted : ");
+    printArray(array, 8);
+    
     return 0;
 }
