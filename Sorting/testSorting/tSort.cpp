c564408b03b7eee525b819c9e5e56fd719894385
Mouhamad <koreissimouhamad@outlook.fr>
2016-11-15 12:05:11 +0100

    Refactored sorting

---
+++ Sorting/testSorting/tSort.cpp
@@ -0,0 +1,18 @@
+#include "../array.cpp"
+
+using namespace std;
+
+int main() {
+    int a[8] = {6, 3, 9, 8, 1, 5, 4, 2};
+    Array<int> array(a, 8);
+    printf("Array before sorting : ");
+    array.print();
+
+    //Sorting the array of size 8
+    array.doSelectionSort();
+
+    printf("Array after being sorted : ");
+    array.print();
+    
+    return 0;
+}
